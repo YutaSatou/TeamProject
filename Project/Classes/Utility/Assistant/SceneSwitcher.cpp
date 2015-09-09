1418c5f092273f21dc5d6680847a3631214384ac
利川聖太 <cassis.0708@gmail.com>
2015-09-09 22:06:32 +0900

    [ add ]シーン変更ラップクラスを作成しました。

---
+++ Project/Classes/Utility/Assistant/SceneSwitcher.cpp
@@ -0,0 +1,32 @@
+#include "SceneSwitcher.h"
+#include "cocos2d.h"
+#include "SceneCreator.h"
+#include "GameMain/GameTitle/GameTitleLayer.h"
+#include "GameMain/GameStageSelect/StageSelectLayer.h"
+#include "GameMain/GamePlay/DebugLayer.h"
+#include "GameMain/GameResult/GameResultLayer.h"
+
+using namespace cocos2d;
+
+// 通常のシーン変更
+void SceneSwitcher::change( SceneType type )
+{
+	using CreateFunction	= std::function< Scene*() >;
+	using CreateFunctionMap	= std::unordered_map< int, CreateFunction >;
+	
+	static CreateFunctionMap convertMap =
+	{
+		{ SceneType::TITLE,			[ & ]()
+			{ return SceneCreator::createScene( GameTitleLayer::create() ); } },
+		{ SceneType::STAGE_SELECT,	[ & ]()
+			{ return SceneCreator::createScene( StageSelectLayer::create() ); } },
+		{ SceneType::PLAY,			[ & ]()
+			{ return SceneCreator::createPhysicsScene( DebugLayer::create(), Vec2( 0.0f, -9.8f ), true, 6.0f ); } },
+		{ SceneType::RESULT,		[ & ]()
+			{ return SceneCreator::createScene( GameResultLayer::create() ); } },
+	};
+	
+	Scene* scene = convertMap.at( type )();
+	
+	Director::getInstance()->replaceScene( scene );
+}
