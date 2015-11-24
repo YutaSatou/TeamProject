79cd4274e0b57fbe59a472aa70d3da1eed87846a
利川聖太 <cassis.0708@gmail.com>
2015-11-24 22:33:42 +0900

    [ add ]ボディデストラクトラッパクラスを追加しました。

---
+++ Project/Classes/GameMain/GamePlay/Object/Common/LiquidFunBodyDeleter.cpp
@@ -0,0 +1,25 @@
+#include "LiquidFunBodyDeleter.h"
+#include "../../LiquidFun/LiquidFunUserAPI.h"
+
+// コンストラクタ
+LiquidFunBodyDeleter::LiquidFunBodyDeleter()
+	: Deleter()
+{
+	registerDeleteFunction( []( LiquidFunBody* body ) { LiquidFunBodySettlor::detachBody( body ); } );
+}
+
+// インスタンスの生成
+LiquidFunBodyDeleter* LiquidFunBodyDeleter::create( LiquidFunBody* body )
+{
+	LiquidFunBodyDeleter* inst = new LiquidFunBodyDeleter();
+	
+	if ( inst && inst->init() )
+	{
+		inst->autorelease();
+		inst->registerDeleteInstance( body );
+		return inst;
+	}
+	
+	CC_SAFE_DELETE( inst );
+	return nullptr;
+}
