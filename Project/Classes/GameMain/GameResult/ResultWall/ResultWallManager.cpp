5f1e09639e9c10a508838eb5945e545fa7a12da2
YutaSatou <yuit1016@gmail.com>
2016-03-01 23:44:55 +0900

    [ modify ]リザルトの演出で溢れていたスライムを入れ物にきちんと入るよう修正しました。

--- Project/Classes/GameMain/GameResult/ResultWall/ResultWallManager.cpp
+++ Project/Classes/GameMain/GameResult/ResultWall/ResultWallManager.cpp
@@ -47,9 +47,9 @@ bool ResultWallManager::init()
 	addChild( left );
 	addChild( right );
 	
-	ResultWall* leftDown { ResultWall::create( { 0, 830 }, { 330, 500 } ) };
+	ResultWall* leftDown { ResultWall::create( { 0, 830 }, { 345, 485 } ) };
 	addChild( leftDown );
-	ResultWall* rightDown { ResultWall::create( { 720, 830 }, { 390, 500 } ) };
+	ResultWall* rightDown { ResultWall::create( { 720, 830 }, { 375, 485 } ) };
 	addChild( rightDown );
 	
 	//ばらばらになったスライムを入れる箱を用意する
